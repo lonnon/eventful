2f8044f90dcbdb15e17ad06ab06284618523db02
Lonnon Foster <lonnon.foster@gmail.com>
2009-09-10 21:39:25 -0700

    Started spec for Eventful tags
    Also cleaned up the event_pages dataset so it's a neatly ordered set of
    typical event pages.

--- spec/datasets/event_pages_dataset.rb
+++ spec/datasets/event_pages_dataset.rb
@@ -7,22 +7,22 @@ class EventPagesDataset < Dataset::Base
                   :class_name  => 'EventPage',
                   :event_start => Time.now + 1.day,
                   :event_end   => Time.now + 1.day + 2.hours
-      (1..5).each do |event|
-        date = 2 * event
+      (2..6).each do |event|
         create_page "Event #{event}",
                     :class_name  => 'EventPage',
-                    :event_start => Time.now + date.days,
-                    :event_end   => Time.now + date.days + 2.hours
+                    :event_start => Time.now + event.days,
+                    :event_end   => Time.now + event.days + 2.hours
       end
       create_page "Old Event",
                   :class_name  => 'EventPage',
                   :event_start => Time.now - 2.days,
                   :event_end   => Time.now - 2.days + 2.hours
       create_page "All-day Event",
-                  :class_name  => 'EventPage'
+                  :class_name  => 'EventPage',
+                  :event_start => Time.now + 7.days
       create_page "No End Specified Event",
                   :class_name  => 'EventPage',
-                  :event_start => Time.now + 2.days
+                  :event_start => Time.now + 8.days
     end
   end
 end
