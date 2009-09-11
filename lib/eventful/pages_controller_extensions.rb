12b0c50102229a77162a1f8c276ecd7d900dd1a7
Lonnon Foster <lonnon.foster@gmail.com>
2009-09-10 19:34:21 -0700

    Ajaxy event check boxes working, after a fashion
    Current implementation uses observe_form, because it appears that
    observe_field doesn't work for check boxes. Checking and unchecking the
    all_day_event and no_end_time check boxes works, but everything goes
    quite pear-shaped if you start typing text into the content fields. The
    javascript observer dutifully sends *all* the text, as it's entered, in
    0.3-second intervals, except that the server round-trip means that each
    0.3-second span takes more like 2-3 seconds, for events that don't
    generate any kind of updates to the page. Ugh.
    
    Possible solutions:
    
    * Code ajaxy bits entirely in javascript using Prototype, possibly with
      help from LowPro. Pro: Everything happens client-side, so there's no
      wait for a server round-trip. Might be able to isolate to check box
      changes only. Con: Not entirely sure how to get all the data shoved
      around without server round trip. It's awfully nice just asking Rails
      to render partials, and I've have to do the equivalent by hand in
      javascript.
    
    * Wrap check boxes in their own form, which is what observe_form
      watches. Pro: Only the changes we care about cause a hit on the
      server. Con: Not sure how to get the data from the check boxes to save
      in the Page model, because the check boxes would now be in a separate
      form. Also, not sure what happens if I try to embed a form within
      another form; might be an HTML no-no.

--- lib/eventful/pages_controller_extensions.rb
+++ lib/eventful/pages_controller_extensions.rb
@@ -1,8 +1,12 @@
 module Eventful::PagesControllerExtensions
   def update_event_ui
+    current_page = Page.new(params[:page])
     @page = Page.find(params[:id])
-    @page.all_day_event = params[:all_day_event]
-    @page.no_end_time = params[:no_end_time]
+    @page.all_day_event = current_page.all_day_event
+    @page.no_end_time = current_page.no_end_time
+##    if current_page.all_day_event != @page.all_day_event ||
+##       current_page.no_end_time != @page.no_end_time
     render :partial => "event_dates_dynamic", :object => @page
+##    end
   end
 end
