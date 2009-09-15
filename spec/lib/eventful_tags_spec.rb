32e395ba99d48fe1ead0b69c97b389befc4de93b
Lonnon Foster <lonnon.foster@gmail.com>
2009-09-14 19:57:00 -0700

    <r:events:each by='field'> implemented, passes spec
    Marked <r:events:each offset='n'> as pending for now. It's not working,
    and I'm not certain why not.

--- spec/lib/eventful_tags_spec.rb
+++ spec/lib/eventful_tags_spec.rb
@@ -44,11 +44,24 @@ describe 'Eventful Tags' do
   end
   
   describe "<r:events:each offset='2'>" do
-    it "should return future event pages starting with the second page"
+    it "should return future event pages starting with the third page" do
+      pending
+      markup = "<r:events:each offset='2'><r:title /> </r:events:each>"
+      
+      expected = "Event 4 Event 5 Event 6 All-day Event No End Specified Event "
+      
+      pages(:event_list).should render(markup).as(expected)
+    end
   end
   
   describe "<r:events:each by='title'>" do
-    it "should return future event pages ordered by title"
+    it "should return future event pages ordered by title" do
+      markup = "<r:events:each by='title'><r:title /> </r:events:each>"
+      
+      expected = "All-day Event Event 2 Event 3 Event 4 Event 5 Event 6 No End Specified Event Normal Event "
+      
+      pages(:event_list).should render(markup).as(expected)
+    end
   end
   
   describe "<r:events:each from='date' to='date'>" do
